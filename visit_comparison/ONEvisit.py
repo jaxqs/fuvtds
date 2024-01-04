f47173da55c28a160ed08da27aa008a8201ff539
Jacqueline Hernandez <jhernandez@stsci.edu>
2024-01-04 16:23:55 -0500

    added original files

---
+++ visit_comparison/ONEvisit.py
@@ -0,0 +1,22 @@
+from data_models import get_new_data
+from func_version2 import plot_flux, plot_net
+from matplotlib.backends.backend_pdf import PdfPages
+
+if __name__ == "__main__":
+    # Change these parameters to what is specific to you
+    PID = '17249'
+    visit = '11'
+
+    data = get_new_data(PID, visit)
+
+    pdf = PdfPages(f'output/{PID}_visit{visit}_flux.pdf')
+    for d in data:
+        plot_flux(d)
+        pdf.savefig()
+    pdf.close()
+
+    pdf = PdfPages(f'output/{PID}_visit{visit}_net.pdf')
+    for d in data:
+        plot_net(d)
+        pdf.savefig()
+    pdf.close()
