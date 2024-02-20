dc569860ed229939fb4824197f69f47bf7916b01
Jacqueline Hernandez <jhernandez@stsci.edu>
2024-02-20 09:13:42 -0500

    added more to the monitor

--- visit_comparison/ONEvisit.py
+++ visit_comparison/ONEvisit.py
@@ -179,8 +179,8 @@ def seperate_segs(data, segment, func):
 
 if __name__ == "__main__":
     # Change these parameters to what is specific to you
-    PID = '17328'
-    visit = '3a'
+    PID = '17326'
+    visit = '04'
 
     data = get_new_data(PID, visit)
 
